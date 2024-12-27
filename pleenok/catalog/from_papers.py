4b2498064536efb65b44fa0a34e3d6f82efac942
Andrea Burattin <andrea.burattin@gmail.com>
2024-12-27 11:55:16 +0100

    Added some attack trees from the Internet

---
+++ pleenok/catalog/from_papers.py
@@ -0,0 +1,13 @@
+from pleenok.model.attack_tree import Gate, GateType
+
+def falco21_fig4() -> Gate:
+	# From https://doi.org/10.1109/SMC-IT51442.2021.00016
+	at = Gate(GateType.SEQUENCE_AND, "Tamper data received from CubeSAT")
+	at1 = at.add_and_gate("Admin access to database server")
+	at1.add_attack("Login to server via SSH")
+	at11 = at1.add_or_gate("Steal credentials to database")
+	at11.add_attack("Search for misconfiguration")
+	at11.add_attack("Get priviledged access to server")
+	at1.add_attack("Login to phpMyAdmin interface")
+	at.add_attack("Change data logged in flight database")
+	return at
